276a415a1a5671f92abf5309fb70e754b51828d1
neofytr <imagination12344321.com@gmail.com>
2025-03-24 19:30:08 +0530

    using sprintf

--- hello.go
+++ hello.go
@@ -7,6 +7,9 @@ func main() {
 	const openRate = 30.5
 
 	msg := "Hi %s, your open rate is %.1f percent\n"
+	msg_s := fmt.Sprintf("Hi %s, your open rate is %.1f percent\n", name, openRate)
+
 	fmt.Printf(msg, name, openRate)
 	fmt.Println("Hi", name+",", "your open rate is", openRate, "percent")
+	fmt.Print(msg_s)
 }
