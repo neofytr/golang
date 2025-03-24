8fe34609da4ff202c0f6c7f637a6033fd5e8a7c2
neofytr <imagination12344321.com@gmail.com>
2025-03-24 19:37:29 +0530

    initializing variables to be in if scope only

--- hello.go
+++ hello.go
@@ -4,11 +4,8 @@ import "fmt"
 
 func main() {
 	messageLen := 10
-	maxMessageLen := 20
 
-	fmt.Println("Trying to send a message of length:", messageLen)
-
-	if messageLen > maxMessageLen {
+	if maxMessageLen := 20; messageLen > maxMessageLen {
 		fmt.Println("Message not sent")
 	} else {
 		fmt.Println("Message sent")
