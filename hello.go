92583e41180fd6ccf7907d7373f585c06b11d5f7
neofytr <imagination12344321.com@gmail.com>
2025-03-24 19:28:47 +0530

    using print and printf and println

--- hello.go
+++ hello.go
@@ -3,8 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	const premiumPlanName = "Premium Plan"
-	const basicPlanName = "Basic Plan"
-	fmt.Println("plan:", premiumPlanName)
-	fmt.Print("plan: ", basicPlanName, "\n")
+	const name = "Saul Goodman"
+	const openRate = 30.5
+
+	msg := "Hi %s, your open rate is %.1f percent\n"
+	fmt.Printf(msg, name, openRate)
+	fmt.Println("Hi", name+",", "your open rate is", openRate, "percent")
 }
