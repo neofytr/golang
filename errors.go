af15d34c867a515cb326045a6685a1c42b7bc80a
neofytr <imagination12344321.com@gmail.com>
2025-03-24 21:14:59 +0530

    multiple return values; named return values

--- errors.go
+++ errors.go
@@ -1,5 +1,16 @@
 package main
 
+func getInfo() (age int, name string) {
+	age = 18
+	name = "raj shukla"
+
+	return age, name
+}
+
 func main() {
-	
+	var age int
+	var name string
+
+	age, name = getInfo()
+	println(age, name)
 }
