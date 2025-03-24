7e9328c399eab288efc9200615f69c5688e0536d
neofytr <imagination12344321.com@gmail.com>
2025-03-24 21:41:58 +0530

    changed names

--- test.go
+++ test.go
@@ -7,11 +7,15 @@ type person_t struct {
 	height float64
 }
 
-func test(info person_t) string {
+func returnInfoString(info person_t) string {
 	return fmt.Sprintf("name: %s, height: %.1f", info.name, info.height)
 }
 
 func main() {
-	var msg string = test(person_t{name: "raj", height: 4.5})
+	raj := person_t{}
+	raj.name = "raj"
+	raj.height = 4.5
+
+	var msg string = returnInfoString(raj)
 	println(msg)
 }
